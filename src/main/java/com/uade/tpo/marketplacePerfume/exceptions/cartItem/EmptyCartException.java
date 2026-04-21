0c7dcecce66c6b3c1c0200b853a679d5a8d7385e
Luca santino Cevasco <thelucazip@gmail.com>
2026-04-20 21:32:31 -0300

    feat(postman): add Checkout endpoint to the Postman collection for cart checkout functionality fix(EmptyCartException.java): update exception message to English for consistency and clarity

--- src/main/java/com/uade/tpo/marketplacePerfume/exceptions/cartItem/EmptyCartException.java
+++ src/main/java/com/uade/tpo/marketplacePerfume/exceptions/cartItem/EmptyCartException.java
@@ -3,6 +3,6 @@ package com.uade.tpo.marketplacePerfume.exceptions.cartItem;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "El carrito esta vacio")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "The cart is empty")
 public class EmptyCartException extends RuntimeException {
 }
